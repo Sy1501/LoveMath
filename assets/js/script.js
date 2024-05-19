8e0fcbea334c1df353778f5d49ae201bed12ef59
Simon Rutledge <sy1501@outlook.com>
2024-05-19 21:01:42 +0000

    added JS for scores not a number issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-// wait for DOM to finshe loading before running the game
-
-// get the button elements anf add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -40,21 +39,25 @@ function runGame(gameType) {
 }
 
 /**
- * Checks the answer agaist the first element in
+ * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
+
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert("Hey! You got it right! :d");
-    }  else {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
-runGame(calculatedAnswer[1]);
+    runGame(calculatedAnswer[1]);
+
 }
 
 /**
@@ -76,12 +79,24 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -99,3 +114,4 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
     
 }
+
